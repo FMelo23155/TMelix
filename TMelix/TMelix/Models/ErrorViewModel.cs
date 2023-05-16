6917f92a9ea33dc6e69d6ac3b3e04f911c3782d5
Tomas23052 <102040688+Tomas23052@users.noreply.github.com>
2023-05-16 22:18:02 +0100

    Inicialização do projeto, criação dos models Filme, Série, Subscrição e Utilizador

---
+++ TMelix/TMelix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TMelix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
