d510f111f8205773c5967bb2437e04074f883876
FMelo23155 <aluno23155@ipt.pt>
2023-05-20 13:21:27 +0100

    Página do Register

--- TMelix/TMelix/Data/Migrations/20230520120620_Inicio2.Designer.cs
+++ TMelix/TMelix/Data/Migrations/20230520120620_Inicio2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TMelix.Data;
 
@@ -11,9 +12,10 @@ using TMelix.Data;
 namespace TMelix.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230520120620_Inicio2")]
+    partial class Inicio2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
