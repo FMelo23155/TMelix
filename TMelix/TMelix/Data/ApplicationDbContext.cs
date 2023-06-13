2aeeb557558868a188e5d4b88bf71715287497e7
Tomas23052 <102040688+Tomas23052@users.noreply.github.com>
2023-06-13 17:22:39 +0100

    criar users e filmes automaticamente com builder

--- TMelix/TMelix/Data/ApplicationDbContext.cs
+++ TMelix/TMelix/Data/ApplicationDbContext.cs
@@ -41,6 +41,105 @@ namespace TMelix.Data
                 new IdentityRole { Id = "s", Name = "Subscritor", NormalizedName = "SUBSCRITOR" }
             );
 
+            builder.Entity<ApplicationUser>().HasData(
+                    new ApplicationUser
+                    {
+                        Nome= "Admin",
+                        Funcao = "Administrador",
+                        Email = "a@a.a",
+                        UserName = "a@a.a",
+                        NormalizedUserName = "A@A.A",
+                        NormalizedEmail = "A@A.A",
+                        Id = "1",
+                        PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(null, "!Admin12345")
+                    }
+                );
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                 {
+                        RoleId = "a", 
+                        UserId = "1" 
+                 }
+            );
+
+            builder.Entity<Serie>().HasData(
+            new Serie
+            {
+                Id = 1,
+                Titulo = "Rabo de Peixe",
+                Imagem = "https://paginaum.pt/wp-content/uploads/2023/06/rabo-de-peixe.jpg",
+                Sinopse = "Um barco carregado de cocaína naufraga nos Açores e Eduardo vê uma oportunidade arriscada, mas empolgante, de ganhar dinheiro e realizar sonhos impossíveis.",
+                DataLancamento = new DateTime(2023, 05, 26),
+                Classificacao = 5,
+                Elenco = "Helena Caldeira, José Condessa ",
+                Genero = "Drama",
+                Temporada = 1,
+                Episodio = 7
+            },
+            new Serie
+            {
+                Id = 2,
+                Titulo = "Suits",
+                Imagem = "https://br.web.img2.acsta.net/pictures/14/03/28/10/18/433386.jpg",
+                Sinopse = "O grande advogado corporativo de Manhattan, Harvey Specter, e sua equipe, Donna Paulsen, Louis Litt e Alex Williams, são lançados em uma disputa pelo poder quando um novo sócio se junta à empresa. Com seus dois melhores associados desaparecidos e Jessica de volta a Chicago, Specter e a equipe tentam se adaptar a uma nova normal sem eles. A equipe enfrenta traições, relacionamentos ardentes e segredos que acabam sendo revelados. Velhas e novas rivalidades vêm à luz entre os membros da equipe.",
+                DataLancamento = new DateTime(2011, 06, 23),
+                Classificacao = 5,
+                Elenco = "Gabriel Macht, Patrick J. Adams",
+                Genero = "Drama, Comédia",
+                Temporada = 9,
+                Episodio = 134
+            },
+            new Serie
+            {
+                Id = 3,
+                Titulo = "Game of Thrones",
+                Imagem = "https://br.web.img3.acsta.net/pictures/19/03/21/16/15/4239577.jpg",
+                Sinopse = "Sucesso entre os livros mais vendidos, a série de obras \"A Song of Ice and Fire\", de George R.R. Martin, é levada à tela quando HBO decide navegar a fundo pelo gênero e recriar a fantasia medieval épica. Este é o retrato de duas famílias poderosas - reis e rainhas, cavaleiros e renegados, homens honestos e mentirosos - disputando um jogo mortal pelo controle dos Sete Reinos de Westeros para assumir o Trono de Ferro.",
+                DataLancamento = new DateTime(2011, 04, 17),
+                Classificacao = 5,
+                Elenco = "Emilia Clarke, Pedro Pascal",
+                Genero = "Fantasia, Drama",
+                Temporada = 8,
+                Episodio = 73
+            }
+            );
+
+            builder.Entity<Filme>().HasData(
+            new Filme
+            {
+                Id = 1,
+                Titulo = "The Little Mermaid",
+                Imagem = "https://m.media-amazon.com/images/M/MV5BYTUxYjczMWUtYzlkZC00NTcwLWE3ODQtN2I2YTIxOTU0ZTljXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+                Sinopse = "Uma jovem sereia faz um acordo com uma bruxa do mar para trocar sua bela voz por pernas humanas para que possa descobrir o mundo acima da água e impressionar um príncipe.",
+                DataLancamento = new DateTime(2023, 05, 25),
+                Classificacao = 2,
+                Elenco = "Halle Bailey, Jonah Hauer-King",
+                Genero = "Fantasia, Musical",
+            },
+            new Filme
+            {
+                Id = 2,
+                Titulo = "Maleficent",
+                Imagem = "https://m.media-amazon.com/images/I/51yXUqukECL._AC_.jpg",
+                Sinopse = "Malévola, uma jovem de coração puro, vive em um pacífico reino na floresta. Um dia, um exército invasor ameaça a harmonia da região e ela se transforma na mais feroz protetora do reino. No entanto, uma terrível traição a transforma em uma mulher amarga e vingativa. Como consequência, ela amaldiçoa Aurora, sua filha recém-nascida. Mas, aos poucos, Malévola percebe que a criança é a chave para a paz no reino e para sua verdadeira felicidade também.",
+                DataLancamento = new DateTime(2014, 06, 05),
+                Classificacao = 4,
+                Elenco = "Angelina Jolie, Ellie Fanning",
+                Genero = "Aventura, Drama",
+            },
+            new Filme
+            {
+                Id = 3,
+                Titulo = "Dumbo",
+                Imagem = "https://br.web.img3.acsta.net/pictures/19/01/17/16/28/3541107.jpg",
+                Sinopse = "     Holt Farrier é uma ex-estrela de circo que retorna da guerra e encontra seu mundo virado de cabeça para baixo. O circo em que trabalhava está passando por grandes dificuldades, e ele fica encarregado de cuidar de um elefante recém-nascido, cujas orelhas gigantes fazem dele motivo de piada. No entanto, os filhos de Holt descobrem que o pequeno elefante é capaz de uma façanha enorme: voar.",
+                DataLancamento = new DateTime(2019, 03, 29),
+                Classificacao = 3,
+                Elenco = "Collin Farrel, Eva Green",
+                Genero = "Aventura, Fantasia",
+            }
+            );
+
             builder.Entity<Filme>()
         .HasMany(f => f.Subscricoes)
         .WithMany(s => s.Filmes)
